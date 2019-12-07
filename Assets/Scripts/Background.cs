c871d963e12f8436f07ca9b7acf834b052e35e65
Duy BUI <bui16910@spengergasse.at>
2019-12-07 21:57:34 +0100

    added moving background

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public float speed = 5;
+    public float clamppos;
+    public Vector3 startpos;
+    void Start()
+    {
+        startpos = transform.position;
+    }
+    void FixedUpdate()
+    {
+        float newpos = Mathf.Repeat(Time.time * speed, clamppos);
+        transform.position = startpos + Vector3.left * newpos;
+    }
+    
+}
---
+++ Assets/Scripts/CamMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    
+    public float speed = 5.0f;
+    void Update()
+    {
+        float xAxisValue = Input.GetAxis("Horizontal");
+        float zAxisValue = Input.GetAxis("Vertical");
+        if (Camera.current != null)
+        {
+            Camera.current.transform.Translate(new Vector3(xAxisValue, 0.0f, zAxisValue));
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    
+}
