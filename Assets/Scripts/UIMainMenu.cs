092e3a4734e894574bc46f7f2235ecb174cff5d6
Duy BUI <bui16910@spengergasse.at>
2019-12-20 00:15:04 +0100

    UI added

---
+++ Assets/Scripts/UIMainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIMainMenu : MonoBehaviour
+{
+	//Play Button - Game Start 
+
+   
+   public void StartGame ()
+   {
+		Debug.Log("Play");
+   	   SceneManager.LoadScene("Map", LoadSceneMode.Single); 
+   }
+   
+
+   //Exit Button - End Game
+
+   public void QuitGame ()
+   {
+		Debug.Log("Quit");
+   	   Application.Quit();
+   }
+
+}
---
+++ Assets/Scripts/UIPauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIPauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false; 
+
+	public GameObject pauseMenuUI;
+
+    public void Resume()
+	{
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		GameIsPaused = false;
+	}
+
+	void Pause()
+	{
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+	}
+	
+	void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			if(GameIsPaused)
+			{
+				Resume();
+			}
+			else 
+			{
+				Pause();
+			}
+		}
+    }
+
+	
+
+}
